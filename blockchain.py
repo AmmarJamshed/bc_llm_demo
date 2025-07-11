7e8f1acbf02bde32c5409920de7f65359e40b18f
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2025-07-11 20:24:11 +0500

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,23 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
+from web3 import Web3
+import os
 
-# In[2]:
+# Connect to Anryton Blockchain (Example RPC URL, replace with actual Anryton RPC)
+rpc_url = os.getenv("RPC_URL", "https://anryton-rpc-url.com")
+w3 = Web3(Web3.HTTPProvider(rpc_url))
 
-
-import hashlib
-import json
-import time
-
-# Simulate Blockchain Storage (In-Memory)
-blockchain_storage = []
-
-def store_on_blockchain(encrypted_query):
-    tx_hash = hashlib.sha256(encrypted_query.encode()).hexdigest()
-    transaction = {
-        "tx_hash": tx_hash,
-        "encrypted_query": encrypted_query,
-        "timestamp": time.time()
+def send_transaction(encrypted_query, from_address, private_key):
+    nonce = w3.eth.getTransactionCount(from_address)
+    tx = {
+        'nonce': nonce,
+        'to': from_address,  # For demo, sending back to self; replace with MOL contract address
+        'value': 0,
+        'gas': 200000,
+        'gasPrice': w3.toWei('5', 'gwei'),
+        'data': encrypted_query.encode('utf-8')
     }
-    blockchain_storage.append(transaction)
-    return tx_hash, transaction
 
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    return w3.toHex(tx_hash)
