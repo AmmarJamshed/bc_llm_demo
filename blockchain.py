af21e37c75d22ef15eea169aed36bbec8682ee51
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2025-07-11 20:41:01 +0500

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -15,5 +15,5 @@ def send_transaction(encrypted_query, from_address, private_key):
         'data': encrypted_query.encode('utf-8')
     }
     signed_tx = w3.eth.account.sign_transaction(tx, private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
     return w3.to_hex(tx_hash)
